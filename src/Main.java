79985902277ee4004dd98e6874f658da84a77056
Prozzo86 <bogdanovic.darijan@gmail.com>
2024-10-27 11:58:31 +0100

    Domaca zadaca statistika rijeci pomocu funkcija

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    /* STATISTIKA RIJECI
+    * Napravi program koji učitava rečenicu i ispisuje:
+    * 1. Sve riječi duže od 5 slova
+    * 2. Najduzu rijec
+    * 3. Prosjecnu duljinu rijeci
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
